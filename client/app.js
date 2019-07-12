c67e1f5836059dc760c91cfee1d07c48e70f15aa
Rivas1 <rivas1@cpp.edu>
2019-07-12 16:41:01 -0700

    Added Dark Theme toggler switch
    Future: Need to store the user's preference into local storage

--- client/app.js
+++ client/app.js
@@ -52,3 +52,17 @@ document.querySelector('#book-list').addEventListener( 'click', (e) => {
 
 
 });
+
+// Event: Toggle dark theme
+const toggleSwitch = document.querySelector('.theme-switch input[type="checkbox"]');
+
+function switchTheme(e) {
+    if (e.target.checked) {
+        document.documentElement.setAttribute('data-theme', 'dark');
+    }
+    else {
+        document.documentElement.setAttribute('data-theme', 'light');
+    }    
+}
+
+toggleSwitch.addEventListener('change', switchTheme, false);
