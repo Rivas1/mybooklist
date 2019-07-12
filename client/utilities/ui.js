aebd37a7c2cdaf3ad915655e4d5aab19fe5f28a7
Rivas1 <rivas1@cpp.edu>
2019-07-12 15:03:33 -0700

    Changed theme to Dark

--- client/utilities/ui.js
+++ client/utilities/ui.js
@@ -15,7 +15,7 @@ export default class UI {
             <td>${book.title}</td>
             <td>${book.author}</td>
             <td>${book.isbn}</td>
-            <td><a href="#" class="btn btn-danger dtn-sm delete">A</a></td>
+            <td><a href="#" class="btn btn-danger btn-sm">Delete</a></td>
         `;
 
         list.appendChild(row);
