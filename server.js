5a398bde8e638e1c9f736bcad1cf79a68ee9d82b
Rivas1 <rivas1@cpp.edu>
2019-07-11 22:21:09 -0700

    Exported logger.js to be used in server.js
    Created a logger.js file that is import into server.js
    
    logger.js logs each http request with a date onto the console

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const path = require('path');
 const bodyParser = require('body-parser');
 const app = express();
 const PORT = process.env.PORT || 5000;
-const moment = require('moment');
+const logger = require('./middleware/logger')
 
 app.use(bodyParser.json())
 app.use(
@@ -12,19 +12,13 @@ app.use(
     })
 );
 
-
-const logger = (request, response, next) => {
-    console.log(`${request.protocol}://${request.get('host')}${request.originalUrl} @${moment().format()}`);
-    next();
-};
-
 // Init middleware
 app.use(logger);
 
 // Gets all books
-app.get('/api/books', (request, response) => {
-    response.json(books)
-});
+// app.get('/api/books', (request, response) => {
+//     response.json(books)
+// });
 
 // Set static folder
 app.use(express.static(path.join(__dirname, 'client')));
