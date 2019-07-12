8287a8c3c87694f004a40a8b08cafa499fe6b630
Rivas1 <rivas1@cpp.edu>
2019-07-12 11:20:00 -0700

    Created a book.js class for cleaner OOP
    I am extracting each class that was embeded in app.js file so create a more robust, modular design.
    I will have each class in it's own unique file.

--- client/app.js
+++ client/app.js
@@ -1,12 +1,4 @@
-
-// Book Class: Represents a Book
-class Book {
-    constructor(title, author, isbn) {
-        this.title = title;
-        this.author = author;
-        this.isbn = isbn;
-    }
-}
+import Book from './models/book.js';
 
 // UI Class: Handle UI Tasks
 class UI {
