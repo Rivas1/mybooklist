b7e652af061edb85ff7102b10f7f451cb5f10698
Rivas1 <rivas1@cpp.edu>
2019-07-12 11:32:08 -0700

    Successfully extracted 'UI' class

--- client/app.js
+++ client/app.js
@@ -1,52 +1,6 @@
 import Book from './models/book.js';
 import Store from './utilities/store.js';
-
-// UI Class: Handle UI Tasks
-class UI {
-    static displayBooks () {
-        const books = Store.getBooks();
-        books.forEach( (book) => UI.addBookToList(book) );
-    }
-
-    static addBookToList(book) {
-        const list = document.querySelector('#book-list'); // grabbing onto the element from the dom
-
-        const row = document.createElement('tr');
-
-        row.innerHTML = `
-            <td>${book.title}</td>
-            <td>${book.author}</td>
-            <td>${book.isbn}</td>
-            <td><a href="#" class="btn btn-danger dtn-sm delete">A</a></td>
-        `;
-
-        list.appendChild(row);
-    }
-
-    static deleteBook(el) {
-        if (el.classList.contains('delete')) {
-            el.parentElement.parentElement.remove();
-        }
-    }
-
-    static showAlert(message, className) {
-        const div = document.createElement('div');
-        div.className = `alert alert-${className}`;
-        div.appendChild(document.createTextNode(message));
-        const container = document.querySelector('.container');
-        const form = document.querySelector('#book-form');
-        container.insertBefore(div, form);
-
-        // Vanishing post 3 seconds
-        setTimeout( () => document.querySelector('.alert').remove(), 3000);
-    }
-
-    static clearFields() {
-        document.querySelector('#title').value = '';
-        document.querySelector('#author').value = '';
-        document.querySelector('#isbn').value = '';
-    }
-}
+import UI from './utilities/ui.js';
 
 // Event: Display Books
 document.addEventListener('DOMContentLoaded', UI.displayBooks);
