e1b7c3c270ee75b4511ca1c7d556809816340801
Rivas1 <rivas1@cpp.edu>
2019-07-15 10:16:11 -0700

    Attention: Needs to save user preference to local storage
    Seems to be something small, address before continuing.
    
    Otherwise, application is where I want it to be.

--- client/app.js
+++ client/app.js
@@ -55,13 +55,16 @@ document.querySelector('#book-list').addEventListener( 'click', (e) => {
 
 // Event: Toggle dark theme
 const toggleSwitch = document.querySelector('.theme-switch input[type="checkbox"]');
+const currentTheme = localStorage.getItem('theme') ? localStorage.getItem('theme') : null;
 
 function switchTheme(e) {
     if (e.target.checked) {
         document.documentElement.setAttribute('data-theme', 'dark');
+        localStorage.setItem('theme', 'dark');
     }
     else {
         document.documentElement.setAttribute('data-theme', 'light');
+        localStorage.setItem('theme', 'light');
     }    
 }
 
